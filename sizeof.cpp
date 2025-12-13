bc8b655d19d78e47a726e664494bfdd9926b7bf5
mislina7 <mislinabb@gmail.com>
2025-12-13 16:41:00 +0300

    Create sizeof.cpp

---
+++ sizeof.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <limits>
+#include <iomanip>
+#include <cstdint>
+
+int main () {
+    using namespace std;
+
+    int file_size = 10;
+    int counter = 0;
+    double price = 8.50;
+    std::cout <<file_size<< std::endl;
+    std::cout <<counter << std::endl;
+    std::cout <<sizeof(file_size)<< " elements\n";
+    return 0;
+}
+
+
+
