0f565745387053f9104313621daaa8e5f834e6f2
mislina7 <mislinabb@gmail.com>
2025-12-15 14:09:06 +0300

    Create robot.h

---
+++ Robot Dead Reckoning Simulator/robot.h
@@ -0,0 +1,29 @@
+#ifndef ROBOT_H
+#define ROBOT_H
+
+namespace robotics {
+
+class Robot {
+private:
+    float x;
+    float  y;
+    float heading;
+public:
+
+    Robot();
+
+    void move(float distance);
+    void rotate(float angle_deg);
+    void reset();
+
+    float getX() const;
+    float getY() const;
+    float getHeading() const;
+
+    void printPose() const;
+
+};
+
+}
+
+#endif
